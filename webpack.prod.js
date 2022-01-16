b448f2c7ec37f47a5fcf8853b7a4ae8af2704aa1
Marcos Campos <marcoscampos.2500@gmail.com>
2022-01-15 22:46:28 -0500

    update wording

--- webpack.prod.js
+++ webpack.prod.js
@@ -10,6 +10,7 @@ const TerserPlugin = require('terser-webpack-plugin');
 module.exports = merge(common, {
   mode: 'production',
   output: {
+    publicPath: '',
     filename: '[name].[contentHash].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
